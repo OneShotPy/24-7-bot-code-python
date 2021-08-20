e32579b5a5cac9cecb6fffd55234af8a10c849fe
OneShotPy <89243364+OneShotPy@users.noreply.github.com>
2021-08-20 13:08:45 +0545

    main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+add the following this
+
+down of import discord
+add 
+import os
+
+//REMOVE THE client.run("Token") Line And Add This
+
+
+keep_alive()
+ 
+TOKEN = os.environ.get("DISCORD_BOT_SECRET")
+ 
+client.run(TOKEN)
+
+
+
+do this all on replit
+add a seceret
+
+name it secret and add token in value
